fcbe5fabd9dec2506fb1a38213f06dfcbe7f6a41
hancygee <lcyhancy666@gmail.com>
2026-04-23 11:31:23 +0000

    Add test cases

---
+++ test_subtest_age.py
@@ -0,0 +1,25 @@
+import unittest
+from age import categorize_by_age
+
+class TestIschild(unittest.TestCase):
+
+    def test_child_age(self):
+        for age in range(0, 9):  # 0-8
+            with self.subTest(age=age):
+                print(f"{age} is considered as a child.")
+                self.assertEqual(categorize_by_age(age), "Child")
+
+    def test_adolescent_age(self):
+        for age in range(10, 19):  # 10-18
+            with self.subTest(age=age):
+                print(f"{age} is considered as an adolescent.")
+                self.assertEqual(categorize_by_age(age), "Adolescent")
+
+    def test_adult_age(self):
+        for age in range(19, 66):  # 19-65
+            with self.subTest(age=age):
+                print(f"{age} is considered as an adult.")
+                self.assertEqual(categorize_by_age(age), "Adult")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
